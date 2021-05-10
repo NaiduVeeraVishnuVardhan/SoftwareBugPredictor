8dc9e1fb798936cda6206edfd6ea0652b4d9b3ba
NaiduVeeraVishnuVardhan <60171846+NaiduVeeraVishnuVardhan@users.noreply.github.com>
2021-05-10 11:40:04 +1000

    add code to run os commands

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,12 +1,32 @@
 const resultPage = (req,res) => {
-    
+
     res.render('result', {title: 'Result Page'})
 }
 
 const repoLink = (req,res) => {
+    
     const repolink = req.body
+
+    const { exec } = require("child_process");
+    console.log('echo'.concat('test') )
+    exec("echo".concat(repolink) , (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+
+
+
+    // const repolink = req.body
     console.log(repolink)
     res.json({ status: repolink })
+
 }
 
 
