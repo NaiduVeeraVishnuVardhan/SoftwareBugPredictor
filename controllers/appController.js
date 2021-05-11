d6b93f2b9a7193a18c645ffd328f3c09f93cec5b
NaiduVeeraVishnuVardhan <60171846+NaiduVeeraVishnuVardhan@users.noreply.github.com>
2021-05-11 13:06:50 +1000

    run commands to capture metrics from the given repo

--- controllers/appController.js
+++ controllers/appController.js
@@ -4,23 +4,41 @@ const resultPage = (req,res) => {
 }
 
 const repoLink = (req,res) => {
-    
+    const shell = require('shelljs')
+    let fs = require("fs")
+
     const repolink = req.body
     //Converting json body to string to access the json data.
-    var string = JSON.stringify(repolink)
-    var obj = JSON.parse(string);
-    var repoLinkk = obj.repolink;
+    const string = JSON.stringify(repolink);
+    const obj = JSON.parse(string);
+    const githubRepoLink = obj.repolink;
+    const repoName = githubRepoLink.split("/").pop().split(".").shift()
+
+    console.log(repoName)
+
 
     directory = process.cwd()
-    fs = require("fs")
+
     if (!fs.existsSync("datacollection")){
         require("fs").mkdirSync("datacollection")
     }
 
-    const shell = require('shelljs')
+    if(fs.existsSync("datacollection/" + repoName)){
+        console.log("I am here")
+        shell.exec("rmdir datacollection/" + repoName)
+    }
+
     shell.cd(directory+"/datacollection")
-    shell.exec('git clone '+repoLinkk)
-    console.log(repoLinkk)
+    shell.exec('git clone '+githubRepoLink)
+
+    shell.cd(directory+"/datacollection/"+repoName)
+    shell.exec('git log --all --numstat --date=short --pretty=format:\'--%h--%ad--%aN\' --no-renames --after=2016-04-01 > logfile.log')
+
+    shell.exec("docker run -v " + directory+"/datacollection/"+repoName + ":/data -it code-maat-app -l /data/logfile.log -c git2 >" + repoName + "_code_metrics_authors.csv")
+    shell.exec("docker run -v " + directory+"/datacollection/"+repoName + ":/data -it code-maat-app -l /data/logfile.log -c git2 -a coupling >" + repoName + "_code_metrics_coupling.csv")
+    shell.exec("docker run -v " + directory+"/datacollection/"+repoName + ":/data -it code-maat-app -l /data/logfile.log -c git2 -a age >" + repoName + "_code_metrics_age.csv")
+
+    console.log("End of the program")
 
     res.json({ status: repolink })
 
