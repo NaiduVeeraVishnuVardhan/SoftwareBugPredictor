49c0484127dc35ee6e9f6167f3c9c5bd13c1e4f5
Mbaireann <mbaireann12@gmail.com>
2021-05-13 09:15:34 +0300

    setup swagger api documentation

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
-const express = require('express');
-const routes = require('./routes/index');
+const express = require("express");
+const routes = require("./routes/index");
 const PORT = 3000;
 const app = express();
-const bodyParser = require('body-parser')
+const swaggerUi = require("swagger-ui-express");
+const { swaggerDocument } = require("./docs/swagger");
+const bodyParser = require("body-parser");
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.use('/public', express.static('public'))
-app.use('/scripts', express.static('scripts'))
-app.set('view engine', 'ejs');
+app.use("/public", express.static("public"));
+app.use("/scripts", express.static("scripts"));
+app.set("view engine", "ejs");
 
-app.use(express.urlencoded( {extended:true} ));
-
-app.get('/', (req, res) => {
-    res.render('home', {title: 'Home Page'});
+app.use(express.urlencoded({ extended: true }));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.get("/", (req, res) => {
+  res.render("home", { title: "Home Page" });
 });
 
 // Routes
-app.use('/', routes)
+app.use("/", routes);
 
 app.use((req, res) => {
-    res.status(404).render('404', {title: '404 Page'});
+  res.status(404).render("404", { title: "404 Page" });
 });
 
 app.listen(PORT);
