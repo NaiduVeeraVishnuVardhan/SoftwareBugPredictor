4a8f16fe283a93aedbf1794952db0b6af689db71
Mbaireann <mbaireann12@gmail.com>
2021-05-16 09:05:39 +0300

    add  sigup and logout api docs

--- docs/auth/index.js
+++ docs/auth/index.js
@@ -1,10 +1,12 @@
 const login =  require('./post-login')
+const signup =  require('./post-signup')
+const logout = require('./get-logout')
 
 module.exports = {
     paths: {
         '/login': { ...login},
         '/user': {},
-        '/logout': {},
-        '/signup': {},
+        '/logout': {...logout},
+        '/signup': {...signup},
     },
 }
