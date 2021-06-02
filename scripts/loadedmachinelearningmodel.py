9b7f68abed9ae5db550ce7278168d3a24b72961d
NaiduVeeraVishnuVardhan <60171846+NaiduVeeraVishnuVardhan@users.noreply.github.com>
2021-06-02 12:39:08 +1000

    Changes in ML model

--- scripts/loadedmachinelearningmodel.py
+++ scripts/loadedmachinelearningmodel.py
@@ -2,12 +2,17 @@
 # load the model from disk
 import pickle
 import pandas as pd
-filename = 'finalized_model.sav'
-loaded_model = pickle.load(open(filename, 'rb'))
 
-testmetrics = pd.read_csv("ml_ready-process-metrics.csv")
-testdata = testmetrics[["age-months","n-authors","n-revs"]]
-Ypred = loaded_model.predict(testdata).round()
-x= testmetrics["entity"].tolist()
-ResultDataFrame = pd.DataFrame({'File name':x, 'predicted bugs':Ypred})
-ResultDataFrame.to_csv("PredictedBugs.csv")
+if __name__ == '__main__':
+
+    filename = 'finalized_model.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+    testmetrics = pd.read_csv("ml_ready-process-metrics.csv")
+    testdata = testmetrics[["age-months","n-authors","n-revs"]]
+    print(testdata)
+    Ypred = loaded_model.predict(testdata).round()
+    print(Ypred)
+    x= testmetrics["entity"].tolist()
+    ResultDataFrame = pd.DataFrame({'File name':x, 'predicted bugs':Ypred})
+    print(ResultDataFrame)
+    ResultDataFrame.to_csv("PredictedBugs.csv")
