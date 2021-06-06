fa7004eaf8f9c11d7c306b6247ad8a392b883a1f
Dhruv <dbparikh@deakin.edu.au>
2021-06-06 19:52:53 +1000

    fixed login error + forgot password

--- controllers/appController.js
+++ controllers/appController.js
@@ -1,11 +1,11 @@
 const ResultModel = require('../models/result');
 
-const resultPage = (req,res) => {
+const resultPage = (req, res) => {
 
-    res.render('result', {title: 'Result Page'})
+    res.render('result', { title: 'Result Page' })
 }
 
-const repoLink = (req,res) => {
+const repoLink = (req, res) => {
     const shell = require('shelljs')
     let fs = require("fs")
 
@@ -20,55 +20,75 @@ const repoLink = (req,res) => {
 
     console.log(repoName)
 
-    
+
     directory = process.cwd()
 
-    if (!fs.existsSync("scripts")){
+    if (!fs.existsSync("scripts")) {
         require("fs").mkdirSync("scripts")
     }
-
-    if(fs.existsSync("scripts/" + repoName)){
+    if (!fs.existsSync("public/files")) {
+        require("fs").mkdirSync("public/files")
+    }
+    if (!fs.existsSync("public/files/predicted_data")) {
+        require("fs").mkdirSync("public/files/predicted_data")
+    }
+    if (!fs.existsSync("public/files/test_data")) {
+        require("fs").mkdirSync("public/files/test_data")
+    }
+    if (fs.existsSync("scripts/" + repoName)) {
         shell.exec("rmdir scripts/" + repoName)
     }
 
-    shell.cd(directory+"/scripts")
-    shell.exec('git clone '+githubRepoLink)
+    shell.cd(directory + "/scripts")
+    shell.exec('git clone ' + githubRepoLink)
 
-    shell.cd(directory+"/scripts/"+repoName)
+    shell.cd(directory + "/scripts/" + repoName)
     shell.exec('git log --all --numstat --date=short --pretty=format:\'--%h--%ad--%aN\' --no-renames --after=2016-04-01 > logfile.log')
     console.log("Created git log for code maat app")
     shell.exec("sudo systemctl start docker")
-    shell.exec("sudo docker run -v " + directory+"/scripts/"+repoName + ":/data -i code-maat-app -l /data/logfile.log -c git2 >" + repoName + "_code_metrics_authors.csv")
-    shell.exec("sudo docker run -v " + directory+"/scripts/"+repoName + ":/data -i code-maat-app -l /data/logfile.log -c git2 -a coupling >" + repoName + "_code_metrics_coupling.csv")
-    shell.exec("sudo docker run -v " + directory+"/scripts/"+repoName + ":/data -i code-maat-app -l /data/logfile.log -c git2 -a age >" + repoName + "_code_metrics_age.csv")
+    shell.exec("sudo docker run -v " + directory + "/scripts/" + repoName + ":/data -i code-maat-app -l /data/logfile.log -c git2 >" + repoName + "_code_metrics_authors.csv")
+    shell.exec("sudo docker run -v " + directory + "/scripts/" + repoName + ":/data -i code-maat-app -l /data/logfile.log -c git2 -a coupling >" + repoName + "_code_metrics_coupling.csv")
+    shell.exec("sudo docker run -v " + directory + "/scripts/" + repoName + ":/data -i code-maat-app -l /data/logfile.log -c git2 -a age >" + repoName + "_code_metrics_age.csv")
     console.log("Created process metrics csv files")
-    shell.cd(directory+"/scripts/")
-    shell.exec("sudo python3 processAndCodemetrics.py --project "+repoName)
-    console.log("Test data is created.")
+    shell.cd(directory + "/scripts/")
+    shell.exec("sudo python3 processAndCodemetrics.py --project " + repoName)
+    shell.exec("sudo python3 loadedmachinelearningmodel.py --project " + repoName)
+    shell.exec("sudo rm -rf " + repoName)
+
+
     res.json({ status: repolink })
 }
 
-const loginPage = (req,res) => {
-    res.render('login' , {title:'Login Page'})
+const loginPage = (req, res) => {
+    res.render('login', { title: 'Login Page' })
 }
-const signupPage = (req,res) => {
-    res.render('signup' , {title:'Sign Up Page'})
+const signupPage = (req, res) => {
+    res.render('signup', { title: 'Sign Up Page' })
+}
+const forgotPasswordPage = (req, res) => {
+    res.render('forgot',
+        {
+            title: 'Resset Pasword  Page',
+            info: false,
+            error: false
+        }
+    )
 }
 
-const addResult = (req,res) => {
+const addResult = (req, res) => {
     const result = new ResultModel({
         name: req.body.name,
         date: Date.now(),
         ownerId: req.user.id
-      });
-    
-      result
+    });
+
+    result
         .save()
         .then((savedItem) => {
-          res.json({savedItem})
+            res.json({ savedItem })
         })
         .catch((err) => {
-          console.error(err);
+            console.error(err);
         });
 }
 
@@ -77,6 +97,7 @@ module.exports = {
     resultPage,
     loginPage,
     signupPage,
+    forgotPasswordPage,
     addResult
 
 }
--- routes/index.js
+++ routes/index.js
@@ -7,16 +7,18 @@ const appController = require('../controllers/appController');
 const authController = require('../controllers/authController');
 const isAuthenticated = require('../helpers/authHelper');
 
-router.post('/api/reg',appController.repoLink);
-router.get('/result',appController.resultPage);
+router.post('/api/reg', appController.repoLink);
+router.get('/result', appController.resultPage);
 router.get('/login', appController.loginPage);
 router.get('/signup', appController.signupPage);
+router.get('/forgot', appController.forgotPasswordPage);
 
 //Auth Routes
 router.post('/login', authController.login);
 router.get('/logout', isAuthenticated, authController.logout);
 router.get('/user', isAuthenticated, authController.getUser);
 router.post('/signup', authController.register);
+router.post('/forgot', authController.forgotPassword);
 
 router.post('/api/reg', appController.repoLink)
 router.get('/result', appController.resultPage)
